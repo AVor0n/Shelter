9f911c860c0037da8c496558aec71226b4c9fbeb
Andrey Voronin <AVor0n@yandex.ru>
2022-04-23 23:59:51 +0300

    feat: update modal

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,38 @@
-import data from './../data/pets.json' assert {type: 'json'};
+import data from './../data/pets.json' assert { type: 'json' };
 const cards = document.querySelectorAll('.slider__card');
 const modal = document.querySelector('.modal');
 const closeBtn = modal.querySelector('.btn');
 
-closeBtn.onclick = () => modal.classList.add('hidden');
+function closeModal() {
+  modal.classList.add('hidden');
+  document.body.style.overflowY = 'auto';
+}
 
-cards.forEach(card => {
-  card.onclick = () => {
-    const animName = card.querySelector('.slider__card-title').textContent;
-    const animImgSrc = card.querySelector('.slider__card-img').src;
-    const animInfo = data.find(it => it.name === animName);
-    if (!animInfo) return;
+function openModal() {
+  modal.classList.remove('hidden');
+  document.body.style.overflowY = 'hidden';
+}
 
-    modal.querySelector('img').src = animImgSrc;
-    const fields = modal.querySelectorAll('[data-prop]');
-    fields.forEach(field => {
-      field.textContent = animInfo[field.dataset.prop];
-    });
-    modal.classList.remove('hidden');
-  };
-});
+function fillCardData(card) {
+  const animName = card.querySelector('.slider__card-title').textContent;
+  const animImgSrc = card.querySelector('.slider__card-img').src;
+  const animInfo = data.find(it => it.name === animName);
+  if (!animInfo) return;
+
+  modal.querySelector('img').src = animImgSrc;
+  const fields = modal.querySelectorAll('[data-prop]');
+  fields.forEach(field => {
+    field.textContent = animInfo[field.dataset.prop];
+  });
+}
+
+cards.forEach(
+  card =>
+    (card.onclick = () => {
+      fillCardData(card);
+      openModal();
+    }),
+);
+
+document.addEventListener('click', e => e.target.classList.contains('modal') && closeModal());
+closeBtn.onclick = () => closeModal();
