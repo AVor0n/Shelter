69e47489fd93bd0014a037ed64385bc2fd8ef237
Andrey Voronin <AVor0n@yandex.ru>
2022-04-26 21:35:21 +0300

    refactor: slider.js

--- js/index.js
+++
@@ -1,6 +0,0 @@
-import {init as initSlider} from './slider.js';
-import './modal.js';
-import './copier.js';
-import './burger.js';
-
-initSlider();
--- js/slider.js
+++ js/slider.js
@@ -1,80 +1,90 @@
-import data from './../data/pets.json' assert { type: 'json' };
+import data from './../data/pets.json' assert {type: 'json'};
+
 const [prevBtn, nextBtn] = document.querySelectorAll('.slider > .btn');
 const slideContainer = document.querySelector('.slider__container');
 
-const cardWidth = 270;
-const cardGap = 90;
-const sliderPadding = 50;
+const CARD_WIDTH = 270;
+const CARD_GAP = 90;
+const SLIDER_PADDING = 50;
+
 let countVisibleCards =
-  document.body.clientWidth <= 760 ? 1 : document.body.clientWidth <= 1260 ? 2 : 3;
-let slideWidth = (cardWidth + cardGap) * countVisibleCards - cardGap + sliderPadding;
-let visibleCards = [];
+    document.body.clientWidth <= 768 ? 1 : document.body.clientWidth <= 1280 ? 2 : 3;
+let slideWidth = getSlideWidth(countVisibleCards);
+let visibleCardNames = [];
 let currentSlide;
 
-window.addEventListener(('resize'), () => {
-  const newCountVisibleCards = document.body.clientWidth <= 760 ? 1 : document.body.clientWidth <= 1260 ? 2 : 3;
-  if(countVisibleCards !== newCountVisibleCards){
-    countVisibleCards = newCountVisibleCards;
-    currentSlide = 0;
-    visibleCards = [];
-    slideWidth = (cardWidth + cardGap) * countVisibleCards - cardGap + sliderPadding;
-    for(const it of slideContainer.children){
-      it.remove()
-    }
-    init();
-  }
-})
+window.addEventListener('load', () => init());
+nextBtn.addEventListener('click', () => switchSlide('right'));
+prevBtn.addEventListener('click', () => switchSlide('left'));
+window.addEventListener('resize', () => {
+    const newCountVisibleCards = document.body.clientWidth <= 760 ? 1 : document.body.clientWidth <= 1260 ? 2 : 3;
+    if (countVisibleCards !== newCountVisibleCards) reInit(newCountVisibleCards);
+});
 
-export function init() {
-  console.log(countVisibleCards);
-  visibleCards = getNewSlideData();
-  currentSlide = createSlide(visibleCards);
+function init() {
+    visibleCardNames = getNewSlideData();
+    currentSlide = createSlide(visibleCardNames);
 }
-
-nextBtn.onclick = () => switchSlide('right');
-prevBtn.onclick = () => switchSlide('left');
 
 function switchSlide(direction) {
-  const newSlideData = getNewSlideData();
-  const newSlide = createSlide(newSlideData, direction);
-  visibleCards = newSlideData;
-  setTimeout(() => {
-    currentSlide.style.left = direction === 'left' ? `${slideWidth}px` : `-${slideWidth}px`;
-    newSlide.style.left = '0';
-    const oldSlide = currentSlide;
-    setTimeout(() => oldSlide.remove(), 1000);
-    currentSlide = newSlide;
-  }, 0);
+    visibleCardNames = getNewSlideData();
+    const newSlide = createSlide(visibleCardNames, direction);
+
+    setTimeout(() => {
+        currentSlide.style.left = direction === 'left' ? `${slideWidth}px` : `-${slideWidth}px`;
+        newSlide.style.left = '0';
+
+        const oldSlide = currentSlide;
+        currentSlide = newSlide;
+
+        setTimeout(() => oldSlide.remove(), 1000);
+    }, 0);
 }
 
-function createCard(name) {
-  const imgSrc = data.find(it => it.name === name).img;
-  const card = document.createElement('div');
-  card.classList.add('slider__card');
-  card.innerHTML = `<img src="${imgSrc}" alt="${name} photo" class="slider__card-img" />
-    <p class="slider__card-title">${name}</p>
-    <button class="btn">Learn more</button>`;
-  return card;
+function getSlideWidth(countVisibleCards) {
+    return (CARD_WIDTH + CARD_GAP) * countVisibleCards - CARD_GAP + SLIDER_PADDING;
 }
 
 function getNewSlideData() {
-  const allowedNames = data.filter(it => !visibleCards.includes(it.name));
-  const nextCards = [];
-  while (nextCards.length < countVisibleCards) {
-    let nextCard = allowedNames[Math.floor(Math.random() * allowedNames.length)].name;
-    if (!nextCards.includes(nextCard)) {
-      nextCards.push(nextCard);
+    const allowedNames = data.filter(it => !visibleCardNames.includes(it.name));
+    const nextCards = [];
+
+    while (nextCards.length < countVisibleCards) {
+        let nextCard = allowedNames[Math.floor(Math.random() * allowedNames.length)].name;
+        if (!nextCards.includes(nextCard))  nextCards.push(nextCard);
     }
-  }
-  return nextCards;
+    return nextCards;
+}
+
+function createCard(name) {
+    const imgSrc = data.find(it => it.name === name).img;
+    const card = document.createElement('div');
+    card.classList.add('slider__card');
+    card.innerHTML = `
+        <img src="${imgSrc}" alt="${name} photo" class="slider__card-img" />
+        <p class="slider__card-title">${name}</p>
+        <button class="btn">Learn more</button>
+    `;
+    return card;
 }
 
 function createSlide(slideData, position) {
-  const slide = document.createElement('div');
-  slide.classList.add('slider__slide');
-  slideData.forEach(it => slide.append(createCard(it)));
-  if (position === 'left') slide.style.left = `-${slideWidth}px`;
-  if (position === 'right') slide.style.left = `${slideWidth}px`;
-  slideContainer.append(slide);
-  return slide;
+    const slide = document.createElement('div');
+    slide.classList.add('slider__slide');
+
+    if (position === 'left') slide.style.left = `-${slideWidth}px`;
+    if (position === 'right') slide.style.left = `${slideWidth}px`;
+
+    slideData.forEach(it => slide.append(createCard(it)));
+    slideContainer.append(slide);
+    return slide;
+}
+
+function reInit(newCountVisibleCards) {
+    Array.from(slideContainer.children).forEach(it => it.remove());
+    slideWidth = getSlideWidth(newCountVisibleCards);
+    countVisibleCards = newCountVisibleCards;
+    visibleCardNames = [];
+    currentSlide = null;
+    init();
 }
